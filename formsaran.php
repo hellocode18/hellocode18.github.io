2a4c146931a360bf415eb9f53c837f30075fa0df
hellocode <hellocode18@gmail.com>
2018-04-07 02:56:27 +0700

    inifile pertama hellocode

---
+++ formsaran.php
@@ -0,0 +1,21 @@
+<?php
+
+
+if (isset($_POST['submit'])) {
+    $saran = $_POST['saran'];
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $univ = $_POST['univ'];
+    $jurusan = $_POST['jurusan'];   
+
+    $mailTo = "wikal.wp@email.unikom.ac.id";
+    $headers = "From: ".$email;
+    $txt ="ada pesan dan kesan baru dari ".$nama.".\n\n".$saran;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
+
+
+
+?>
